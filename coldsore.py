aa2bbab75de3252bcb0e536e0ca64a8e89803061
Osamuede Obasuyi <80005871+OObasuyi@users.noreply.github.com>
2024-04-18 16:34:18 +0200

    fix pull ise

--- coldsore.py
+++ coldsore.py
@@ -18,12 +18,12 @@ class ColdSore:
     
     def pull_tenable_info(self):
         tenable_info = self.config['TENABLE']
-        tenable_info = Session()
-        tenable_info.verify = self.ssl_verify
-        tenable_info.headers = {"Accept": "application/json", "Content-Type": "application/json"}
-        tenable_info.headers["x-apikey"] = f"accesskey={tenable_info['accesskey']}; secretkey={tenable_info['secretkey']}"
+        tenable_session = Session()
+        tenable_session.verify = self.ssl_verify
+        tenable_session.headers = {"Accept": "application/json", "Content-Type": "application/json"}
+        tenable_session.headers["x-apikey"] = f"accesskey={tenable_info['accesskey']}; secretkey={tenable_info['secretkey']}"
         score_data_url = f'{tenable_info["node"]}/rest/analysis'
-        sc_pd = pd.DataFrame([]) #holder
+        sc_pd = pd.DataFrame([]) # holder
 
         req_score_data = {
             "type": "vuln",
@@ -31,16 +31,16 @@ class ColdSore:
             "sourceType": "cumulative"
         } 
         # pull data from SC
-        data_req = tenable_info.post(score_data_url, data=req_score_data)
+        data_req = tenable_session.post(score_data_url, data=dumps(req_score_data))
         if data_req.status_code != 200:
             self.logger.critical(f'tenable: could not reach node at {score_data_url}')
             self.logger.critical(f'tenable: code recvd from node {data_req.status_code} \n\n CONTENT:\n\n\ {data_req.content}\n\n')
             self.logger.critical(f'tenable: QUITTING PROGRAM!!!!!!')
             quit()
         else:
-            sc_dat = json.loads(data_req.content).get("response").get('results')
+            sc_dat = loads(data_req.content).get("response").get('results')
         
-        sc_pd = pd.Dataframe(sc_dat)
+        sc_pd = pd.DataFrame(sc_dat)
         # normalize and remove unneeded.
         return sc_pd
 
